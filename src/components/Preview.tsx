15b0ba6d6c851c47d22ca40d83d862233c655315
Jarrison Cano <me@jarrison.dev>
2024-06-06 19:26:39 -0500

    fix: markdown highlighting

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,14 +1,45 @@
 import ReactMarkdown from 'react-markdown'
 import { useSectionsStore } from '../store/useSections'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export const Preview = () => {
   const { sections } = useSectionsStore()
 
+  const markdown = sections.map((section) => section.content).join('\n')
+
   return (
     <div className="flex w-full flex-col gap-4 pl-4">
-      {sections.map((section) => (
-        <ReactMarkdown key={section.id}>{section.content}</ReactMarkdown>
-      ))}
+      <ReactMarkdown
+        className={'markdown'}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          code({ className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+
+            return match ? (
+              <SyntaxHighlighter
+                // @ts-expect-error - I don't know how to fix this
+                style={dracula}
+                PreTag="div"
+                language={match[1]}
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   )
 }
